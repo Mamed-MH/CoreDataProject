9cba6171c627d2ed1c8f2b420ceaebf94974c653
Mamed-MH <muhammedmz0619@gmail.com>
2024-11-09 13:05:56 +0400

    Initial Commit

---
+++ CoreDataProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataProject
+//
+//  Created by Mamed Hacıyev on 9.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
